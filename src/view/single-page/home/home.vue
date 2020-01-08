5170cd8b9084bcc1fc39612f3b550d5917af2be4
huxiong <huxiong@utry.cn>
2020-01-08 09:53:19 +0800

    主页更新

--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -1,7 +1,9 @@
 <template>
     <div style="height: 300px;margin-top: 5px">
         <div style="display: flex">
-            <div class="index-displayCard">
+            <div class="index-displayCard"
+                 onMouseOver="this.style='box-shadow: -1px 1px 2px 0.5px #c5c5c5;border: 0;transform:translateY(-1px)'"
+                 onmouseout="this.style='box-shadow: 0;border: 0.5px solid #dfdfdf'">
                 <div class="left" style="background-color: #ff7751">
                     <Icon type="md-bonfire" style="color: white" size="33"/>
                 </div>
@@ -12,46 +14,61 @@
                             <Icon type="ios-arrow-up" style="width: 9px;"/>
                             3
                         </div>
-                        <!--                        <div style="align-self: flex-end;margin-bottom: 3px;color: red">-->
-                        <!--                            <Icon type="ios-arrow-down" style="width: 9px;"/>-->
-                        <!--                            3-->
-                        <!--                        </div>-->
                     </div>
                     <div style="display: flex;justify-content: center;width: 100%;color: grey">总计用户数</div>
                 </div>
             </div>
-<!--            <div class="index-displayCard">-->
-<!--                <div class="left">-->
-<!--                    <Icon type="md-brush" size="33"/>-->
-<!--                </div>-->
-<!--                <div class="right">-->
-<!--                    12-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="index-displayCard">-->
-<!--                <div class="left">-->
-<!--                    <Icon type="md-bulb" size="33"/>-->
-<!--                </div>-->
-<!--                <div class="right">-->
-<!--                    12-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="index-displayCard">-->
-<!--                <div class="left">-->
-<!--                    <Icon type="md-desktop" size="33"/>-->
-<!--                </div>-->
-<!--                <div class="right">-->
-<!--                    12-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="index-displayCard">-->
-<!--                <div class="left">-->
-<!--                    <Icon type="logo-rss" size="33"/>-->
-<!--                </div>-->
-<!--                <div class="right">-->
-<!--                    12-->
-<!--                </div>-->
-<!--            </div>-->
+            <div class="index-displayCard"
+                 onMouseOver="this.style='box-shadow: -1px 1px 2px 0.5px #c5c5c5;border: 0;transform:translateY(-1px)'"
+                 onmouseout="this.style='box-shadow: 0;border: 0.5px solid #dfdfdf'">
+                <div class="left" style="background-color: #ff7751">
+                    <Icon type="md-brush" style="color: white" size="33"/>
+                </div>
+                <div style="width: 60%">
+                    <div style="display: flex;justify-content: center;">
+                        <div class="right">22</div>
+                        <div style="align-self: flex-end;margin-bottom: 3px;color: red">
+                            <Icon type="ios-arrow-down" style="width: 9px;"/>
+                            3
+                        </div>
+                    </div>
+                    <div style="display: flex;justify-content: center;width: 100%;color: grey">总计用户数</div>
+                </div>
+            </div>
+            <div class="index-displayCard"
+                 onMouseOver="this.style='box-shadow: -1px 1px 2px 0.5px #c5c5c5;border: 0;transform:translateY(-1px)'"
+                 onmouseout="this.style='box-shadow: 0;border: 0.5px solid #dfdfdf'">
+                <div class="left" style="background-color: #ff7751">
+                    <Icon type="md-desktop" style="color: white" size="33"/>
+                </div>
+                <div style="width: 60%">
+                    <div style="display: flex;justify-content: center;">
+                        <div class="right">22</div>
+                        <div style="align-self: flex-end;margin-bottom: 3px;color: red">
+                            <Icon type="ios-arrow-down" style="width: 9px;"/>
+                            3
+                        </div>
+                    </div>
+                    <div style="display: flex;justify-content: center;width: 100%;color: grey">总计用户数</div>
+                </div>
+            </div>
+            <div class="index-displayCard"
+                 onMouseOver="this.style='box-shadow: -1px 1px 2px 0.5px #c5c5c5;border: 0;transform:translateY(-1px)'"
+                 onmouseout="this.style='box-shadow: 0;border: 0.5px solid #dfdfdf'">
+                <div class="left" style="background-color: #ff7751">
+                    <Icon type="logo-rss" style="color: white" size="33"/>
+                </div>
+                <div style="width: 60%">
+                    <div style="display: flex;justify-content: center;">
+                        <div class="right">22</div>
+                        <div style="align-self: flex-end;margin-bottom: 3px;color: red">
+                            <Icon type="ios-arrow-down" style="width: 9px;"/>
+                            3
+                        </div>
+                    </div>
+                    <div style="display: flex;justify-content: center;width: 100%;color: grey">总计用户数</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -74,14 +91,15 @@
     }
 
     .index-displayCard {
-        width: 20%;
-        max-width: 300px;
+        width: 25%;
+        max-width: 400px;
         display: flex;
         flex-direction: row;
         height: 65px;
         border-radius: 5px;
         margin: 6px;
-        box-shadow: -1px 1px 2px 0.5px #c5c5c5;
+        border: 0.5px solid #dfdfdf;
+        user-select: none;
 
         .left {
             width: 40%;
