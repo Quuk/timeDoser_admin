2a32a5df6f7a3353e0bf5e548ad8138d6d523d78
tcyeee <tcyeee@outlook.com>
2020-06-21 14:52:18 +0800

    更新登录方法

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -16,27 +16,24 @@
 </template>
 
 <script>
-import LoginForm from '_c/login-form'
-import { mapActions } from 'vuex'
+  import LoginForm from '_c/login-form'
+  import {mapActions} from 'vuex'
 
-export default {
-  components: {
-    LoginForm
-  },
-  methods: {
-    ...mapActions([
-      'handleLogin',
-      'getUserInfo'
-    ]),
-    handleSubmit ({ userName, password }) {
-      this.handleLogin({ userName, password }).then(res => {
-        // this.$router.push({
-        //   name: this.$config.homeName
-        // })
-      })
+  export default {
+    components: {
+      LoginForm
+    },
+    methods: {
+      ...mapActions(['handleLogin']),
+      handleSubmit({userName, password}) {
+        this.handleLogin({userName, password}).then(res => {
+          // this.$router.push({
+          //   name: this.$config.homeName
+          // })
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
